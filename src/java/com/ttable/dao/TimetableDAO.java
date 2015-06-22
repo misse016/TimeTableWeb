0fdf3a16c7d33397ac0a08e38ed959c03e3a61ec
Marcello MISSE <misse016@gmail.com>
2015-06-22 08:32:42 +0100

    Timetable DAO class Update

--- src/java/com/ttable/dao/TimetableDAO.java
+++ src/java/com/ttable/dao/TimetableDAO.java
@@ -5,7 +5,10 @@
  */
 package com.ttable.dao;
 
+import com.ttable.model.Course;
 import com.ttable.model.Day;
+import com.ttable.model.Lecturer;
+import com.ttable.model.Period;
 import com.ttable.model.Timetable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -28,35 +31,32 @@ public class TimetableDAO extends DAO<Timetable> {
     @Override
     public boolean insert(Timetable obj) {
         try {
-            PreparedStatement preparedStatement = 
-                    connection.prepareStatement("INSERT INTO timetable(course_code, "
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement("INSERT INTO timetable(course_code, "
                             + "period_id, classroom_id, day_name) VALUES(?,?,?,?)");
-            
-            if (obj.getCourse()==null)
-                preparedStatement.setNull(1,1);
-            else
+
+            if (obj.getCourse() == null) {
+                preparedStatement.setNull(1, 1);
+            } else {
                 preparedStatement.setString(1, obj.getCourse().getCourseCode());
+            }
             preparedStatement.setInt(2, obj.getPeriod().getPeriodId());
             preparedStatement.setString(3, obj.getClassroom().getClassroomId());
             preparedStatement.setString(4, obj.getDay().getDayName());
-            
+
             preparedStatement.executeUpdate();
-        } catch(SQLException e) {
-           e.printStackTrace(); 
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return true;   
+        return true;
     }
 
     @Override
     public boolean delete(Timetable obj) {
         try {
             PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM timetable WHERE course_code=?"
-                            + "and period_id=? and classroom_id=? and day_name=?");
-            preparedStatement.setString(1, obj.getCourse().getCourseCode());
-            preparedStatement.setInt(2, obj.getPeriod().getPeriodId());
-            preparedStatement.setString(3, obj.getClassroom().getClassroomId());
-            preparedStatement.setString(4, obj.getDay().getDayName());
+                    connection.prepareStatement("DELETE FROM timetable WHERE timetable_id=?");
+            preparedStatement.setInt(1, obj.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -76,7 +76,32 @@ public class TimetableDAO extends DAO<Timetable> {
 
     @Override
     public Timetable getById(int id) {
-        return null;
+        Timetable timetable = new Timetable();
+        try {
+            PreparedStatement preparedStatement = 
+                    connection.prepareStatement("SELECT * FROM timetable WHERE timetable_id=?");
+            preparedStatement.setInt(1, id);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                timetable.setId(rs.getInt("timetable_id"));
+
+                CourseDAO courseDAO = new CourseDAO(this.connection);
+                timetable.setCourse(courseDAO.getById(rs.getString("course_code")));
+
+                PeriodDAO periodDAO = new PeriodDAO(this.connection);
+                timetable.setPeriod(periodDAO.getById(rs.getInt("period_id")));
+
+                ClassroomDAO classroomDAO = new ClassroomDAO(this.connection);
+                timetable.setClassroom(classroomDAO.getById(rs.getString("classroom_id")));
+
+                DayDAO dayDAO = new DayDAO(this.connection);
+                timetable.setDay(dayDAO.getById(rs.getString("day_name")));
+            } 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return timetable;
     }
 
     @Override
@@ -84,30 +109,112 @@ public class TimetableDAO extends DAO<Timetable> {
         List<Timetable> timetables = new ArrayList<Timetable>();
         try {
             Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery("SELECT * FROM timetable");
+            ResultSet rs = statement.executeQuery("SELECT * FROM timetable");
             while (rs.next()) {
                 Timetable timetable = new Timetable();
-                
+
                 timetable.setId(rs.getInt("timetable_id"));
-                
+
                 CourseDAO courseDAO = new CourseDAO(this.connection);
                 timetable.setCourse(courseDAO.getById(rs.getString("course_code")));
-                
+
                 PeriodDAO periodDAO = new PeriodDAO(this.connection);
                 timetable.setPeriod(periodDAO.getById(rs.getInt("period_id")));
-                
+
                 ClassroomDAO classroomDAO = new ClassroomDAO(this.connection);
                 timetable.setClassroom(classroomDAO.getById(rs.getString("classroom_id")));
-                
+
                 DayDAO dayDAO = new DayDAO(this.connection);
                 timetable.setDay(dayDAO.getById(rs.getString("day_name")));
-                
+
                 timetables.add(timetable);
             }
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return timetables;
-    } 
+    }
+
+    public Timetable getByCourseCode(String courseCode) {
+        Timetable timetable = null;
+        try {
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement("SELECT * FROM timetable WHERE course_code=?");
+            preparedStatement.setString(1, courseCode);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                timetable = new Timetable();
+                timetable.setId(rs.getInt("timetable_id"));
+
+                CourseDAO courseDAO = new CourseDAO(this.connection);
+                timetable.setCourse(courseDAO.getById(rs.getString("course_code")));
+
+                PeriodDAO periodDAO = new PeriodDAO(this.connection);
+                timetable.setPeriod(periodDAO.getById(rs.getInt("period_id")));
+
+                ClassroomDAO classroomDAO = new ClassroomDAO(this.connection);
+                timetable.setClassroom(classroomDAO.getById(rs.getString("classroom_id")));
+
+                DayDAO dayDAO = new DayDAO(this.connection);
+                timetable.setDay(dayDAO.getById(rs.getString("day_name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return timetable;
+    }
+
+    public List<Timetable> getIndividualList(Lecturer lecturer) {
+        List<Timetable> timetables = new ArrayList<Timetable>();
+        
+        if (!lecturer.getListCourse().isEmpty()) {
+            for (int i = 0; i < lecturer.getListCourse().size(); i++) {
+                
+                if (getByCourseCode(lecturer.getListCourse().get(i).getCourseCode())!=null) {
+                    timetables.add(getByCourseCode(lecturer.getListCourse().get(i).getCourseCode()));
+                }
+            }
+        }
+        return timetables;
+    }
+
+    public List<Timetable> getBlockedList(Day day, Period period) {
+        List<Timetable> timetables = new ArrayList<Timetable>();
+        try {
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement("SELECT * FROM timetable "
+                            + "WHERE day_name=? and period_id=?");
+            preparedStatement.setString(1, day.getDayName());
+            preparedStatement.setInt(2, period.getPeriodId());
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                Timetable timetable = new Timetable();
+
+                timetable.setId(rs.getInt("timetable_id"));
+
+                CourseDAO courseDAO = new CourseDAO(this.connection);
+                timetable.setCourse(courseDAO.getById(rs.getString("course_code")));
+
+                PeriodDAO periodDAO = new PeriodDAO(this.connection);
+                timetable.setPeriod(periodDAO.getById(rs.getInt("period_id")));
+
+                ClassroomDAO classroomDAO = new ClassroomDAO(this.connection);
+                timetable.setClassroom(classroomDAO.getById(rs.getString("classroom_id")));
+
+                DayDAO dayDAO = new DayDAO(this.connection);
+                timetable.setDay(dayDAO.getById(rs.getString("day_name")));
+
+                timetables.add(timetable);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return timetables;
+    }
 }
